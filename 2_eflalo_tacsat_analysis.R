3d598a7dac895a203ea00e6ac2ff6139be293d32
Roi Martinez <roimarful@gmail.com>
2025-03-21 15:01:41 +0000

    Comments and modifications to replace the SplitAmongPings2 function

--- 2_eflalo_tacsat_analysis.R
+++ 2_eflalo_tacsat_analysis.R
@@ -10,7 +10,7 @@ for(year in yearsToSubmit){
   print(paste0("Start loop for year ",year))
   
   #'----------------------------------------------------------------------------
-  # 2.1.0 load TACSAT and EFLALO data from file                             ----
+  # 2.1  load TACSAT and EFLALO data from file                             ----
   #'----------------------------------------------------------------------------
   load(file = paste0(outPath,paste0("/cleanEflalo",year,".RData")) )
   load(file = paste0(outPath, paste0("/cleanTacsat", year,".RData")) )
@@ -19,73 +19,110 @@ for(year in yearsToSubmit){
   tacsat$geometry <- NULL
   
   #'----------------------------------------------------------------------------
-  # 2.1.1 Merge TACSAT and EFLALO                                             ----
+  # 2.2  Assign EFLALO Fishing trip information (gear, vessel, lenght, etc. ) to VMS records in TACSAT                                           ----
   #'----------------------------------------------------------------------------
-  tacsatp <- mergeEflalo2Tacsat(eflalo,tacsat)
-  
   
   #'----------------------------------------------------------------------------
-  # 2.1.2 Assign gear and length                                              ----
+  # 2.2.1  Assign EFLALO Fishing Trip identifiers to TACSAT records                                           ----
   #'----------------------------------------------------------------------------
-  # Define the columns to be added
-  cols <- c("LE_GEAR", "LE_MSZ", "VE_LEN", "VE_KW", "LE_RECT", "LE_MET", "LE_WIDTH", "VE_FLT", "VE_COU")
+  #'
+  #'Assign a EFLALO trip identifier (FT_REF)  at each VMS record in TACSAT . 
+  #'Methods asign fishing trip to VMS records which  date/time  is between the Trip dates of departure and return to port  .
+  #'VMSTools::mergeEflalo2Tacsat function 
   
-  # Use a loop to add each column
-  for (col in cols) {
-    # Match 'FT_REF' values in 'tacsatp' and 'eflalo' and use these to add the column from 'eflalo' to 'tacsatp'
-    tacsatp[[col]] <- eflalo[[col]][match(tacsatp$FT_REF, eflalo$FT_REF)]
-  }
-
-  tacsatp <- data.frame(tacsatp)
+    tacsatp <- mergeEflalo2Tacsat(eflalo,tacsat)
+    tacsatp <- data.frame(tacsatp)
   
-  # Save not merged tacsat data
-  # Subset 'tacsatp' where 'FT_REF' equals 0 (not merged)
-  tacsatpmin <- subset(tacsatp, FT_REF == 0)
   
-  # Feedback on tacsatpmin
-  cat(sprintf("%.2f%% of of the tacsat data did not merge\n", (nrow(tacsatpmin) / (nrow(tacsatpmin) + nrow(tacsatp))) * 100))
   
-  # Save 'tacsatpmin' to a file named "tacsatNotMerged<year>.RData" in the 'outPath' directory
-  save(
-    tacsatpmin,
-    file = file.path(outPath, paste0("tacsatNotMerged", year, ".RData"))
-  )
+  # Filter TACSAT data with assigned EFLALO fishing trips identifiers ----
+   
   
-  # Subset 'tacsatp' where 'FT_REF' does not equal 0 (merged)
-  tacsatp <- subset(tacsatp, FT_REF != 0)
+    # Save not merged tacsat data
+    # Subset 'tacsatp' where 'FT_REF' equals 0 (not merged)
   
+    tacsatpmin <- subset(tacsatp, FT_REF == 0)
+    
+    #' Attention: Check the number of records that were not assigned with a Fishing trip identifier from EFLALO. 
+    #' A large proportion of VMS records matched with a FT_REF value (FT_REF == 0) indicates something wrong 
+    #' Review dates/time fields content and formats both in EFLALO and TACSAT 
+    
+    cat(sprintf("%.2f%% of of the tacsat data did not merge\n", (nrow(tacsatpmin) / (nrow(tacsatpmin) + nrow(tacsatp))) * 100))
+  
+    # Save 'tacsatpmin' to a file named "tacsatNotMerged<year>.RData" in the 'outPath' directory
+    save(
+      tacsatpmin,
+      file = file.path(outPath, paste0("tacsatNotMerged", year, ".RData"))
+    )
+    
+    
+    
+    # Subset TACSAT (tacsatp)  records  with Fishing Trip identifiers assigned 
+    #' FT_REF distinct to  0 , measn records where successfully assigned with FT_REF identifier
+    #' Attention: Only the data with assigned FT_REF is retained for further analysis
+    
+    tacsatp <- subset(tacsatp, FT_REF != 0)
+    
   #'----------------------------------------------------------------------------
-  # 2.1.3 For multi gear/metier etc trips, divide the pings to the right gear/metier etc. ----
+  # 2.2.2 Assign EFLALO - Fishing Trip information ( e.g. gear and length ) to TACSAT records  ----
   #'----------------------------------------------------------------------------
-
-  tacsatpa_LE_GEAR <- trip_assign(tacsatp, eflalo, col = "LE_GEAR", trust_logbook = T)
-  tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_GEAR$FT_REF,], tacsatpa_LE_GEAR), fill = T)
+   
+    #'----------------------------------------------------------------------------
+    # 2.2.2.1 Assign Fishing Trip and Vessel Details at Trip Level
+    #'----------------------------------------------------------------------------
+    #' The gear , mesh size, used during a fishing trip ,  the ICES rectangle reported and 
+    #' Vessel Characteristics are assigned to each VMS record part of a Fishign Trip. 
+    #' Attention: Over 12 m commonly fish in several ICES Rectangles during a trip and 
+    #' also could  use different gears during a trip. See section 2.2.2.2
+    
+    # Define the columns to be added
+    cols <- c("LE_GEAR", "LE_MSZ", "VE_LEN", "VE_KW", "LE_RECT", "LE_MET", "LE_WIDTH", "VE_FLT", "VE_COU")
+    
+    # Use a loop to add each column
+    for (col in cols) {
+      # Match 'FT_REF' values in 'tacsatp' and 'eflalo' and use these to add the column from 'eflalo' to 'tacsatp'
+      tacsatp[[col]] <- eflalo[[col]][match(tacsatp$FT_REF, eflalo$FT_REF)]
+    }
+    
   
-  tacsatpa_LE_MSZ <- trip_assign(tacsatp, eflalo, col = "LE_MSZ", trust_logbook = T)
-  tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_MSZ$FT_REF,], tacsatpa_LE_MSZ), fill = T)
-  
-  tacsatpa_LE_RECT <- trip_assign(tacsatp, eflalo, col = "LE_RECT", trust_logbook = T)
-  tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_RECT$FT_REF,], tacsatpa_LE_RECT), fill = T)
-  
-  tacsatpa_LE_MET <- trip_assign(tacsatp, eflalo, col = "LE_MET", trust_logbook = T)
-  tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_MET$FT_REF,], tacsatpa_LE_MET), fill = T)
-  
-  if("LE_WIDTH" %in% names(eflalo)){
-    tacsatpa_LE_WIDTH <- trip_assign(tacsatp, eflalo, col = "LE_WIDTH", trust_logbook = T)
-    tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_WIDTH$FT_REF,], tacsatpa_LE_WIDTH), fill = T)
-  }
-  
-  #Set catch date to be equal to SI_DATE 
-  tacsatp$LE_CDAT <- tacsatp$SI_DATE
-  
-  tacsatp <- as.data.frame(tacsatp)
-  
-  # Save 'tacsatp' to a file named "tacsatMerged<year>.RData" in the 'outPath' directory
-  save(
-    tacsatp,
-    file = file.path(outPath, paste0("tacsatMerged", year, ".RData"))
-  )
-  
+      #'----------------------------------------------------------------------------
+      # 2.2.2.2 Assign to TACSAT the Fishing Trips using more than one gear and fishing in several ICES Rectangles   
+      #'----------------------------------------------------------------------------
+      #' For trips using more than one fear ( mesh size/metier) and fishign in more than one ICES Rectangles
+      #' The gears and ICES Rectangle are assigned to TACSAT using the LE_CDAT date (Fishing-Log Event Date) 
+      #' that match the TACSAT/VMS date/time record date . 
+      #' Attention: It is common that VMS records part of a fishing trip , do not have match the dates
+      #' recorded in LE_CDAT attribute. The function "trip_assign" ensure to assign the "most used" gear or
+      #' "most visited" ICES rectangles to the TACSAT records with not matchLE_CDAT within a trip. This ensure 
+      #' all TACSAT records part of Fishin Trip have an assigned value. 
+    
+      tacsatpa_LE_GEAR <- trip_assign(tacsatp, eflalo, col = "LE_GEAR", trust_logbook = T)
+      tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_GEAR$FT_REF,], tacsatpa_LE_GEAR), fill = T)
+      
+      tacsatpa_LE_MSZ <- trip_assign(tacsatp, eflalo, col = "LE_MSZ", trust_logbook = T)
+      tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_MSZ$FT_REF,], tacsatpa_LE_MSZ), fill = T)
+      
+      tacsatpa_LE_RECT <- trip_assign(tacsatp, eflalo, col = "LE_RECT", trust_logbook = T)
+      tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_RECT$FT_REF,], tacsatpa_LE_RECT), fill = T)
+      
+      tacsatpa_LE_MET <- trip_assign(tacsatp, eflalo, col = "LE_MET", trust_logbook = T)
+      tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_MET$FT_REF,], tacsatpa_LE_MET), fill = T)
+      
+      if("LE_WIDTH" %in% names(eflalo)){
+        tacsatpa_LE_WIDTH <- trip_assign(tacsatp, eflalo, col = "LE_WIDTH", trust_logbook = T)
+        tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_WIDTH$FT_REF,], tacsatpa_LE_WIDTH), fill = T)
+      }
+      
+      
+      
+      tacsatp <- as.data.frame(tacsatp)
+      
+      # Save 'tacsatp' to a file named "tacsatMerged<year>.RData" in the 'outPath' directory
+      save(
+        tacsatp,
+        file = file.path(outPath, paste0("tacsatMerged", year, ".RData"))
+      )
+      
   
   #'----------------------------------------------------------------------------
   # 2.1.4 Define activity                                                   ----
@@ -331,9 +368,11 @@ for(year in yearsToSubmit){
   )
   
   message("Defining activity completed")
+  
+  
 
-  # 2.2 Dispatch landings of merged eflalo at the ping scale
-  # -------------------------------------------------
+  # 2.2 Dispatch EFLALO landings at VMS position scale ( SplitAmongPing)
+  # ------------------------------------------------------------------------
     
   #'----------------------------------------------------------------------------
   # 2.2.1 continued, filter out invalid metier level 6 codes                           
@@ -342,56 +381,57 @@ for(year in yearsToSubmit){
     removed <- nrow(tacsatp %>% filter(LE_MET %!in% valid_metiers))
     tacsatp <- tacsatp %>% filter(LE_MET %in% valid_metiers)
     cat(sprintf("%.2f%% of of the tacsatp removed due to invalid metier l6 \n", (removed / (removed + kept) * 100)))
-
-  #'----------------------------------------------------------------------------
-  # 2.2.2 Calculate total landings weight and sales value 
+  
+  # 2.2.2 Dispatch landings of merged eflalo at the ping scale
   # -------------------------------------------------
+    
+    # Input data preparation
+    
+    
+    ## 2.2.2.1 Creates EFLALO LE_KG_TOT and LE_EURO_TO if not created yet. 
+    
+      #' Attention: Only applies if you have EFLALO LE_KG and LE_EURO by species . If not species columns and 
+      #' LE_KG_TOT and LE_KG_EURO are already calculated this section wont change the data 
+    
+      # Get the indices of columns in eflalo that contain "LE_KG_" or "LE_EURO_"
+      idx_kg <- grep("LE_KG_", colnames(eflalo)[colnames(eflalo) %!in% c("LE_KG_TOT")])
+      idx_euro <- grep("LE_EURO_", colnames(eflalo)[colnames(eflalo) %!in% c("LE_EURO_TOT")])
+      
+      # Calculate the total KG and EURO for each row
+      if("LE_KG_TOT" %!in% names(eflalo))
+        eflalo$LE_KG_TOT <- rowSums(eflalo[, idx_kg], na.rm = TRUE)
+      if("LE_EURO_TOT" %!in% names(eflalo))
+        eflalo$LE_EURO_TOT <- rowSums(eflalo[, idx_euro], na.rm = TRUE)
   
-  # Get the indices of columns in eflalo that contain "LE_KG_" or "LE_EURO_"
-  idx_kg <- grep("LE_KG_", colnames(eflalo)[colnames(eflalo) %!in% c("LE_KG_TOT")])
-  idx_euro <- grep("LE_EURO_", colnames(eflalo)[colnames(eflalo) %!in% c("LE_EURO_TOT")])
-  
-  # Calculate the total KG and EURO for each row
-  if("LE_KG_TOT" %!in% names(eflalo))
-    eflalo$LE_KG_TOT <- rowSums(eflalo[, idx_kg], na.rm = TRUE)
-  if("LE_EURO_TOT" %!in% names(eflalo))
-    eflalo$LE_EURO_TOT <- rowSums(eflalo[, idx_euro], na.rm = TRUE)
-  
-  # Remove the columns used for the total calculation
-   eflalo <- eflalo[, -c(idx_kg, idx_euro)]
+      # Remove the columns used for the total calculation
+      eflalo <- eflalo[, -c(idx_kg, idx_euro)]
+      
+      
+    ## 2.2.2.2 Retain EFLALO/LB records with related TACSAT/VMS records in EFLALOM ( Eflalo Merged)
+      
+      #' Only records in EFLALOM are taking forward for further analysis 
+      #' The EFLALO/Logbook records with not related VMS records are retained in EFLALONM ( Eflalo Not Merged)
+      #' Attention: Only Logbook records with related VMS ( Fishing or not fishing ) are retained for further analysis
+      
+      eflaloM  <- subset(eflalo, FT_REF %in% unique(tacsatp$FT_REF))
+      eflaloNM <- subset(eflalo, !FT_REF %in% unique(tacsatp$FT_REF))
 
+      #' Attention: Check the number of records in EFLALOM and EFLALONM are reasonable. 
+      #' e.g. Considering the EFLALO records are mainly related to fleet of over 12 meters vessels
+      #' the majority of the EFLALO/LB records must have related VMS records
+       
+      message(sprintf("%.2f%% of the eflalo data not in tacsat\n", (nrow(eflaloNM) / (nrow(eflaloNM) ))))
 
-  #'----------------------------------------------------------------------------
-  # 2.2.3 Retain only EFLALO fishing trip records with related  VMS Records  (EFLALOM) 
-  # --------------------------------------------------------------------------
-  
-  eflaloNM <- subset(eflalo, !FT_REF %in% unique(tacsatp$FT_REF))
-  eflaloM <- subset(eflalo, FT_REF %in% unique(tacsatp$FT_REF))
-
-  message(sprintf("%.2f%% of the eflalo data not in tacsat\n", (nrow(eflaloNM) / (nrow(eflaloNM) ))))
-
-  #'----------------------------------------------------------------------------
-  # 2.2.4 Retain only VMS Records identified  as fishing  operations
-  # --------------------------------------------------------------------------          
-                              
+      
+    ## 2.2.2.3 Filter the 
+      
   # Convert SI_STATE to binary (0/1) format
   tacsatp$SI_STATE <- ifelse(tacsatp$SI_STATE == "f", 1, 0)
   
   # Filter rows where SI_STATE is 1
   tacsatEflalo <- tacsatp[tacsatp$SI_STATE == 1,]
-
-
-  #'----------------------------------------------------------------------------
-  # 2.2.5 Retain only VMS Records with associated effort ( INTV ) 
-  # --------------------------------------------------------------------------   
   
   tacsatp <- tacsatp[!is.na(tacsatp$INTV),]
-
-  #'----------------------------------------------------------------------------
-  # 2.2.6 Split landings among VMS records pings 
-  # --------------------------------------------------------------------------   
-
- 
   
   # Distribute landings among pings, first by day, metier and trip; then by metier and trip; then by trip
   tacsatEflalo <- splitAmongPings2(tacsatp, eflalo)
@@ -416,28 +456,22 @@ for(year in yearsToSubmit){
   
   print("")
   
-}  ##End of the  loop
+}
 
 
 
 #'------------------------------------------------------------------------------
-# 2.3 Add auxiliary spatial information to tacsatEflalo   and data preparation before data aggregation      ----
+# 2.3 Add information to tacsatEflalo                                     ----
 #'------------------------------------------------------------------------------
-                              
 # If you already have cleaned tacsatEflalo files elsewhere, 
 # change file location below, and make sure data is called tacsatEflalo
 # Loop trough years to submit
-                              
 for(year in yearsToSubmit){
-
-#'------------------------------------------------------------------------------
-# 2.3.1 Add auxiliary spatial information to tacsatEflalo                                     ----
-#'------------------------------------------------------------------------------
-  
   print(paste0("Start loop for year ",year))
   load(file = paste0(outPath,"tacsatEflalo",year,".RData"))
   
- 
+  # 2.3.1 Assign c-square, year, month, quarter, area and create table 1
+  # ------------------------------------------------------------------
   # Add habitat and bathymetry to the tacsatEflalo file
   tacsatEflalo <- tacsatEflalo |> 
     sf::st_as_sf(coords = c("SI_LONG", "SI_LATI"), remove = F) |> 
@@ -446,32 +480,17 @@ for(year in yearsToSubmit){
     st_join(bathy, join = st_intersects) |> 
     mutate(geometry = NULL) |> 
     data.frame()
-
-#'------------------------------------------------------------------------------
-# 2.3.2 Add CSquare geocode to  tacsatEflalo   VMS records                    ----
-#'------------------------------------------------------------------------------
   
   # Calculate the c-square based on longitude and latitude
   tacsatEflalo$Csquare <- CSquare(tacsatEflalo$SI_LONG, tacsatEflalo$SI_LATI, degrees = 0.05)
-
-#'------------------------------------------------------------------------------
-# 2.3.3 Calcualte time fields and  convert effort indicators    to Hours      ----
-#'------------------------------------------------------------------------------
   
   # Extract the year and month from the date-time
   tacsatEflalo$Year <- year(tacsatEflalo$SI_DATIM)
   tacsatEflalo$Month <- month(tacsatEflalo$SI_DATIM)
-
-  ### ATTENTION: ONLY RUN THIS CODE IF YOUR EFFORT IS IN CALCUALTED MINUTES!!!!!!
   
   # Calculate the kilowatt-hour and convert interval to hours
   tacsatEflalo$kwHour <- tacsatEflalo$VE_KW * tacsatEflalo$INTV / 60
   tacsatEflalo$INTV <- tacsatEflalo$INTV / 60
-
-
-#'------------------------------------------------------------------------------
-# 2.3.4 Calculate SWEPT AREA   by VMS record in TACSATEFLALO                   ----
-#'------------------------------------------------------------------------------
   
   # Add the calculated gear width to each fishing point
   tacsatEflalo$GEARWIDTH <- add_gearwidth(tacsatEflalo)
